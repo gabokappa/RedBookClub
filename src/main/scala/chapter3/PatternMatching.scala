edcf36edb5bb5b15057e121e46d10d3f9e46a4aa
Gabriel Gonzalez <gabokappa@gmail.com>
2020-03-02 19:58:03 +0000

    Ch3.7

--- src/main/scala/chapter3/PatternMatching.scala
+++ src/main/scala/chapter3/PatternMatching.scala
@@ -75,6 +75,18 @@ object PatternMatching extends App {
       }
     }
 
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+      as match {
+        case Nil => z
+        case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      }
+
+    def sum2(ns: List[Int]) =
+      foldRight(ns, 0)((x, y) => x + y)
+
+    def product2(ns: List[Double]) =
+      foldRight(ns, 1.0)(_ * _)
+
 
 
 
