c4e344fdaa4d6c79f6a57289c1d107f8922e913e
Gabriel Gonzalez <gabokappa@gmail.com>
2020-03-12 16:09:51 +0000

    Commented out ch4

--- src/main/scala/chapter4/ErrorsAndExceptions.scala
+++ src/main/scala/chapter4/ErrorsAndExceptions.scala
@@ -1,20 +1,55 @@
-package chapter4
-
-object ErrorsAndExceptions extends App {
-
-  sealed trait Option[+A] {
-
-    def map[B](f: A => B): Option[B]
-    def flatMap [B](f: A => Option[B]): Option[B]
-    def getOrElse[B >: A](default: => B): B
-    def orElse[B >: A](ob: => Option[B]): Option[B]
-    def filter(f: A => Boolean): Option[A]
-
-
-  }
-
-  case class Some[+A](get: A) extends Option[A]
-  case object None extends Option[Nothing]
-
-
-}
+//package chapter4
+//
+//object ErrorsAndExceptions extends App {
+//
+//  sealed trait Option[+A] {
+//
+//    def map[B](f: A => B): Option[B] = this match {
+//      case None => None
+//      case Some(x) => Some(f(x))
+//    }
+//
+//    def flatMap[B](f: A => Option[B]): Option[B] =
+//      map(f)getOrElse(None)
+//
+//    def getOrElse[B >: A](default: => B): B = this match {
+//      case None => default
+//      case Some(x) => x
+//    }
+//
+//
+//    def orElse[B >: A](ob: => Option[B]): Option[B] =
+//      this.map(Some(_)).getOrElse(ob)
+//
+//    def filter(f: A => Boolean): Option[A] =
+//      flatMap(a => if (f(a)) Some(a) else None)
+//
+//    def filter2(f: A => Boolean): Option[A] = this match{
+//      case Some(a) if f(a) => this
+//      case _ => None
+//    }
+//
+//    def mean(xs: Seq[Double]): Option[Double] =
+//      if (xs.isEmpty) None
+//      else Some(xs.sum / xs.length)
+//
+//    def variance(xs: Seq[Double]): Option[Double] =
+//      mean(xs) flatMap (m => mean(xs.map(x => math.pow(x -m, 2))))
+//
+//    def lift[A, B] (f: A => B): Option[A] => Option[B] = _.map(f)
+//
+//    val absO: Option[Double] => Option[Double] = lift(math.abs)
+//
+//    def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double]
+//
+//
+//
+//
+//
+//  }
+//
+//  case class Some[+A](get: A) extends Option[A]
+//  case object None extends Option[Nothing]
+//
+//
+//}
---
+++ src/test/scala/chapter4/ErrorsAndExceptionsSpec.scala
@@ -0,0 +1,10 @@
+//package chapter4
+//
+//import chapter4.ErrorsAndExceptions.Option
+//import org.scalatest.freespec.AnyFreeSpec
+//import org.scalatest.matchers.should.Matchers
+//
+//class ErrorsAndExceptionsSpec extends AnyFreeSpec with Matchers {
+//
+//
+//}
