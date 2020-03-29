381b178f67387e8146a3272cdafeabde10becb67
Gabriel Gonzalez <gabokappa@gmail.com>
2020-03-29 12:21:01 +0100

    start of ch5

--- src/main/scala/chapter4/ErrorsAndExceptions.scala
+++ src/main/scala/chapter4/ErrorsAndExceptions.scala
@@ -1,55 +1,98 @@
-//package chapter4
-//
-//object ErrorsAndExceptions extends App {
-//
-//  sealed trait Option[+A] {
-//
-//    def map[B](f: A => B): Option[B] = this match {
-//      case None => None
-//      case Some(x) => Some(f(x))
-//    }
-//
-//    def flatMap[B](f: A => Option[B]): Option[B] =
-//      map(f)getOrElse(None)
-//
-//    def getOrElse[B >: A](default: => B): B = this match {
-//      case None => default
-//      case Some(x) => x
-//    }
-//
-//
-//    def orElse[B >: A](ob: => Option[B]): Option[B] =
-//      this.map(Some(_)).getOrElse(ob)
-//
-//    def filter(f: A => Boolean): Option[A] =
-//      flatMap(a => if (f(a)) Some(a) else None)
-//
-//    def filter2(f: A => Boolean): Option[A] = this match{
-//      case Some(a) if f(a) => this
-//      case _ => None
-//    }
-//
-//    def mean(xs: Seq[Double]): Option[Double] =
-//      if (xs.isEmpty) None
-//      else Some(xs.sum / xs.length)
-//
-//    def variance(xs: Seq[Double]): Option[Double] =
-//      mean(xs) flatMap (m => mean(xs.map(x => math.pow(x -m, 2))))
-//
-//    def lift[A, B] (f: A => B): Option[A] => Option[B] = _.map(f)
-//
-//    val absO: Option[Double] => Option[Double] = lift(math.abs)
-//
-//    def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double]
-//
-//
-//
-//
-//
-//  }
-//
-//  case class Some[+A](get: A) extends Option[A]
-//  case object None extends Option[Nothing]
-//
-//
-//}
+package chapter4
+
+
+object ErrorsAndExceptions extends App {
+
+  sealed trait Option[+A] {
+
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(x) => Some(f(x))
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      map(f)getOrElse(None)
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(x) => x
+    }
+
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+      this.map(Some(_)).getOrElse(ob)
+
+    def filter(f: A => Boolean): Option[A] =
+      flatMap(a => if (f(a)) Some(a) else None)
+
+    def filter2(f: A => Boolean): Option[A] = this match{
+      case Some(a) if f(a) => this
+      case _ => None
+    }
+
+    def mean(xs: Seq[Double]): Option[Double] =
+      if (xs.isEmpty) None
+      else Some(xs.sum / xs.length)
+
+    def variance(xs: Seq[Double]): Option[Double] =
+      mean(xs) flatMap (m => mean(xs.map(x => math.pow(x -m, 2))))
+
+    def lift[A, B] (f: A => B): Option[A] => Option[B] = _.map(f)
+
+    val absO: Option[Double] => Option[Double] = lift(math.abs)
+
+    def Try[A](a: => A): Option[A] =
+      try Some(a)
+    catch { case e: Exception => None }
+
+    def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double =
+      (numberOfSpeedingTickets / age) * 2.0
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try(age.toInt)
+    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+
+    map2(optAge, optTickets)(insuranceRateQuote)
+  }
+
+    def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+      a flatMap(aa => b map(bb => f(aa, bb)))
+
+
+    def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap(hh => sequence(t) map(hh :: _))
+    }
+
+
+    def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+      case Nil => Some(Nil)
+      case h :: t => map2(f(h), traverse(t)(f))( _ :: _)
+    }
+
+    def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x => x)
+
+    def altMap2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+      for {
+        aa <- a
+        bb <- b
+      } yield f(aa, bb)
+
+
+  }
+
+  sealed trait Either[+E, +A]
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](values: A) extends Either[Nothing, A]
+
+
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
+
+  object Option {
+
+
+
+
+  }
+}
---
+++ src/main/scala/chapter5/Streams.scala
@@ -0,0 +1,40 @@
+package chapter5
+
+sealed trait Stream[+A] {
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+
+}
+
+case object Empty extends Stream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  def cons[A](hd: => A, t1: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = t1
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = {
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+  }
+
+}
+
+object main extends App {
+
+ val a =  Stream(1, 2, 3, 4, 5)
+
+  println(a.toList)
+  println(s"this is a ${a}")
+
+}
